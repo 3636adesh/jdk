03c6772bdcfb96ae90ee406873a7fd504653a5ab
adesh <3636adesh@gmail.com>
2024-10-30 15:27:29 +0530

    Start kr diya hai....

---
+++ chalo_kuch_tufani_karte_hai/src/main/java/com/mountain_dew/ChaloKuchTufaniKarteHaiApplication.java
@@ -0,0 +1,13 @@
+package com.mountain_dew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaloKuchTufaniKarteHaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaloKuchTufaniKarteHaiApplication.class, args);
+	}
+
+}
---
+++ chalo_kuch_tufani_karte_hai/src/test/java/com/mountain_dew/ChaloKuchTufaniKarteHaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mountain_dew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaloKuchTufaniKarteHaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
